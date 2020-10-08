ff638c32cefc6d5e894fbf90f74bb278bd26db08
HediDar <HediDar@users.noreply.github.com>
2020-10-08 12:31:22 +0000

    Automated deployment: Thu Oct  8 12:31:22 UTC 2020 HediDar/story-book@6b0662594afbd7419bc2e1c4ebbd10f2b6c4bc35

---
+++ precache-manifest.c7bd311bd3c9edd63fd67f0619eb15af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "262452cc8dc7a8958631f412b7d6293b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f9383eaf71c483002607",
+    "url": "/static/css/main.75c945f9.chunk.css"
+  },
+  {
+    "revision": "ea766eede18ce7252c78",
+    "url": "/static/js/2.aa5f12b9.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.aa5f12b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9383eaf71c483002607",
+    "url": "/static/js/main.8163996e.chunk.js"
+  },
+  {
+    "revision": "88627bc5489ded1eecc3",
+    "url": "/static/js/runtime-main.1bc61dce.js"
+  }
+]);
